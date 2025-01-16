56c20a9c777b97f540bc4f71df66da5c9fc80324
Naman2004singh <naman.2singh9@gmail.com>
2025-01-17 02:32:07 +0530

    updated the responsive handlerfile to check for user login status on different screens

--- lib/responsive_handler.dart
+++ lib/responsive_handler.dart
@@ -1,10 +1,37 @@
+import 'package:budget_app/mobile/expense_view_mobile.dart';
+import 'package:budget_app/mobile/login_view_mobile.dart';
 import 'package:budget_app/view_model.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:budget_app/web/expense_view_web.dart';
+import 'package:budget_app/web/login_view_web.dart';
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ResponsiveHandler extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    throw UnimplementedError();
+    final viewModelProvider = ref.watch(viewModel);
+    viewModelProvider.isLoggedIn();
+
+    if (viewModelProvider.isSignedIn == true) {
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return ExpenseViewWeb();
+          } else {
+            return ExpenseViewMobile();
+          }
+        },
+      );
+    } else {
+      return LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 600) {
+            return LoginViewWeb();
+          } else {
+            return LoginViewMobile();
+          }
+        },
+      );
+    }
   }
 }
